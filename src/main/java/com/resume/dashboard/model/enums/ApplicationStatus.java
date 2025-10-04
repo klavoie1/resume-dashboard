a3588b7b6e41a19c94e2ea2a2c34c3af73afd7c0
klavoie <klavoi8@wgu.edu>
2025-10-04 16:27:47 -0400

    created application statuses

---
+++ src/main/java/com/resume/dashboard/model/enums/ApplicationStatus.java
@@ -0,0 +1,9 @@
+package com.resume.dashboard.model.enums;
+
+public enum ApplicationStatus {
+    UNKNOWN,             // Will be used as my default value
+    PENDING,
+    ACCEPTED,
+    REJECTED,
+    IN_PROGRESS
+}
