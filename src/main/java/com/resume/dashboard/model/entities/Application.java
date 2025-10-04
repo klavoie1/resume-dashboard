a11370863dab9ccd15c8e67ff6960093cebb0256
klavoie <klavoi8@wgu.edu>
2025-10-04 18:23:50 -0400

    created fields for application class

--- src/main/java/com/resume/dashboard/model/entities/Application.java
+++ src/main/java/com/resume/dashboard/model/entities/Application.java
@@ -1,4 +1,44 @@
 package com.resume.dashboard.model.entities;
 
+import com.resume.dashboard.model.enums.ApplicationStatus;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "applications")
+@EntityListeners(AuditingEntityListener.class)
 public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "job_title", nullable = false)
+    private String jobTitle;
+
+    @Column(name = "company", nullable = false)
+    private String company;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ApplicationStatus status;
+
+    @Column(name = "submission_date")
+    private LocalDate submissionDate;
+
+    @Column(name = "progress")
+    private Integer progress;
+
+    @CreatedDate
+    @Column(name = "created_date",  nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_date", nullable = false)
+    private LocalDateTime updatedDate;
 }
