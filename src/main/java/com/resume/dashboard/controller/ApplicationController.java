d0188e0b31fa17ed2472e02e068d0ff9041cce84
klavoie <klavoi8@wgu.edu>
2025-10-04 19:06:05 -0400

    application mappings in controller

--- src/main/java/com/resume/dashboard/controller/ApplicationController.java
+++ src/main/java/com/resume/dashboard/controller/ApplicationController.java
@@ -1,4 +1,36 @@
 package com.resume.dashboard.controller;
 
+import com.resume.dashboard.model.entities.Application;
+import com.resume.dashboard.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ApplicationController {
+
+    @Autowired
+    private ApplicationService applicationService;
+
+    @PostMapping("/applications")
+    public Application createApplication(@RequestBody Application application) {
+        return applicationService.save(application);
+    }
+
+    @GetMapping("/applications")
+    public List<Application> getAllApplications() {
+        return applicationService.findAll();
+    }
+
+    @PutMapping("/applications/{id}")
+    public Application updateApplication(@RequestBody Application application, @PathVariable("id") Long id) {
+        return applicationService.updateApplication(application, id);
+    }
+
+    @DeleteMapping("/applications/{id}")
+    public String deleteApplication(@PathVariable("id") Long id) {
+        applicationService.deleteApplicationById(id);
+        return "Application has been deleted";
+    }
 }
