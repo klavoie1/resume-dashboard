5e0b75d582b6da2c1b25ce5d0711b6de97d24972
klavoie <klavoi8@wgu.edu>
2025-10-04 18:50:38 -0400

    added service interface

--- src/main/java/com/resume/dashboard/service/ApplicationService.java
+++ src/main/java/com/resume/dashboard/service/ApplicationService.java
@@ -1,4 +1,19 @@
 package com.resume.dashboard.service;
 
+import com.resume.dashboard.model.entities.Application;
+import java.util.List;
+import java.util.Optional;
+
 public interface ApplicationService {
+
+    Application save(Application application);
+
+    List<Application> findAll();
+
+    Optional<Application> findById(Long id);
+
+    Application updateApplication(Application application, Long id);
+
+    void deleteApplicationById(Long id);
+
 }
