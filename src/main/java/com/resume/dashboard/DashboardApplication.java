b21a3c522530d5ce8e5f2ff9e6297dd34777a1c6
klavoie <klavoi8@wgu.edu>
2025-10-04 18:25:16 -0400

    added jpa auditing annotation for time keeping

--- src/main/java/com/resume/dashboard/DashboardApplication.java
+++ src/main/java/com/resume/dashboard/DashboardApplication.java
@@ -2,12 +2,12 @@ package com.resume.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DashboardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DashboardApplication.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(DashboardApplication.class, args); }
 
 }
