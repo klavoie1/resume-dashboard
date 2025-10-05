fc92d728e4a29963f1adcb429302f9c5256ce756
klavoie <klavoi8@wgu.edu>
2025-10-05 18:14:43 -0400

    added accessors

--- src/UI/src/app/core/models/application.ts
+++ src/UI/src/app/core/models/application.ts
@@ -1,3 +1,10 @@
 export class Application {
-
+  id?: number;
+  jobTitle?: string;
+  company?: string;
+  status?: "UNKNOWN" | "PENDING" | "ACCEPTED" | "REJECTED" | "IN_PROGRESS";
+  submissionDate?: string;
+  progress?: number;
+  createdDate?: string;
+  updatedDate?: string;
 }
