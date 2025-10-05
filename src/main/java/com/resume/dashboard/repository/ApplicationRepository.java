800fcf07b903a0830269d4cb306d030b66a6ebf0
klavoie <klavoi8@wgu.edu>
2025-10-05 16:43:55 -0400

    changed to CrudRepository

--- src/main/java/com/resume/dashboard/repository/ApplicationRepository.java
+++ src/main/java/com/resume/dashboard/repository/ApplicationRepository.java
@@ -1,7 +1,7 @@
 package com.resume.dashboard.repository;
 
 import com.resume.dashboard.model.entities.Application;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface ApplicationRepository extends JpaRepository<Application, Long> { }
+public interface ApplicationRepository extends CrudRepository<Application, Long> { }
 
