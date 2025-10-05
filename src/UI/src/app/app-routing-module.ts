1eced4e0577940048a4eef132afe21ba0197973a
klavoie <klavoi8@wgu.edu>
2025-10-04 21:58:50 -0400

    routed the dashboard component

--- src/UI/src/app/app-routing-module.ts
+++ src/UI/src/app/app-routing-module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/dashboard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: Dashboard },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
