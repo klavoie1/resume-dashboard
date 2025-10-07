5312367715f8e4d95402d0bddbdf13bc1aa262ce
klavoie <klavoi8@wgu.edu>
2025-10-07 13:33:05 -0400

    changed crudrepo to jparepo; added missing annotation

--- src/main/java/com/resume/dashboard/repository/ApplicationRepository.java
+++ src/main/java/com/resume/dashboard/repository/ApplicationRepository.java
@@ -1,7 +1,9 @@
 package com.resume.dashboard.repository;
 
 import com.resume.dashboard.model.entities.Application;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ApplicationRepository extends CrudRepository<Application, Long> { }
+@Repository
+public interface ApplicationRepository extends JpaRepository<Application, Long> { }
 
