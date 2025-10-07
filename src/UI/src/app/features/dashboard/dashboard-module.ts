fe0b2b6e7af1a8c8260cb1b2745baed4caffdeab
klavoie <klavoi8@wgu.edu>
2025-10-07 13:23:40 -0400

    import and declaring ngx chart dependencies

--- src/UI/src/app/features/dashboard/dashboard-module.ts
+++ src/UI/src/app/features/dashboard/dashboard-module.ts
@@ -5,17 +5,23 @@ import { DashboardRoutingModule } from './dashboard-routing-module';
 import { Dashboard } from './dashboard';
 import {AppModule} from '../../app-module';
 import {ApplicationList} from '../application-list/application-list';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxChartsModule } from "@swimlane/ngx-charts";
+import {DashboardChart} from '../dashboard-chart/dashboard-chart';
 
 
 @NgModule({
   declarations: [
     Dashboard,
     ApplicationList,
+    DashboardChart
   ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    AppModule
+    AppModule,
+    BrowserAnimationsModule,
+    NgxChartsModule,
   ]
 })
 export class DashboardModule { }
