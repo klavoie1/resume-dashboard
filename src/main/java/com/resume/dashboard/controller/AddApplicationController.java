40f64987fbd5f61a89a23f1d0ce5c709188d6231
klavoie <klavoi8@wgu.edu>
2025-10-07 13:32:27 -0400

    initial file for new application controller

---
+++ src/main/java/com/resume/dashboard/controller/AddApplicationController.java
@@ -0,0 +1,9 @@
+package com.resume.dashboard.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class AddApplicationController {
+}
