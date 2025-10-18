5c322ad7eac0841d2b3a1309f4cfa83d3548bc92
klavoie <klavoi8@wgu.edu>
2025-10-18 10:19:39 -0400

    deleted unused file

--- src/main/java/com/resume/dashboard/controller/AddApplicationController.java
+++
@@ -1,9 +0,0 @@
-package com.resume.dashboard.controller;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@CrossOrigin(origins = "http://localhost:4200")
-public class AddApplicationController {
-}
