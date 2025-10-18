9d4ab70821f39561336fbec337128db25c0e0e19
klavoie <klavoi8@wgu.edu>
2025-10-17 20:57:50 -0400

    initial commit

---
+++ src/UI/src/app/features/add-application/add-application.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-application',
+  standalone: false,
+  templateUrl: './add-application.html',
+  styleUrl: './add-application.css'
+})
+export class AddApplication {
+
+}
