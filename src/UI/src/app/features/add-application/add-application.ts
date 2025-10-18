7db9814d923128e919d138e4c2e6fb005b1293e0
klavoie <klavoi8@wgu.edu>
2025-10-18 10:19:19 -0400

    Created default form, added logic for submitting form

--- src/UI/src/app/features/add-application/add-application.ts
+++ src/UI/src/app/features/add-application/add-application.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApplicationService } from '../../core/services/application-service';
+import { Application } from '../../core/models/application';
 
 @Component({
   selector: 'app-add-application',
@@ -7,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrl: './add-application.css'
 })
 export class AddApplication {
+  application: Application = {
+    jobTitle: '',
+    company: '',
+    status: 'PENDING',
+    submissionDate: ''
+  };
+  isSubmitting = false;
+  error?: string;
 
+  constructor(private applicationService: ApplicationService, private router: Router) {}
+
+  onSubmit() {
+    this.error = undefined;
+    this.isSubmitting = true;
+    this.applicationService.save(this.application).subscribe({
+      next: () => {
+        this.isSubmitting = false;
+        this.router.navigate(['/dashboard']); // Navigates back to the Dashboard view, could change to success message
+      },
+      error: (err) => {
+        this.isSubmitting = false;
+        this.error = 'Failed to save application';
+        console.error(err);
+      }
+    });
+  }
 }
