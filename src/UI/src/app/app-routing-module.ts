1034d1bb75425028878fae5ae74358cc93bf3625
klavoie <klavoi8@wgu.edu>
2025-10-17 20:55:06 -0400

    Fixed redirect paths and added a catch all path

--- src/UI/src/app/app-routing-module.ts
+++ src/UI/src/app/app-routing-module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Dashboard } from './features/dashboard/dashboard';
+import { AddApplication } from './features/add-application/add-application';
 
 const routes: Routes = [
   { path: 'dashboard', component: Dashboard },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  { path: 'add-application', component: AddApplication },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: '/dashboard' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
