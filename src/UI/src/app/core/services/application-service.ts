b06d6cad21f909286fde5dc92aaa362a7559cb0e
klavoie <klavoi8@wgu.edu>
2025-10-18 10:20:46 -0400

    Corrected the application-service so better match CRUD

--- src/UI/src/app/core/services/application-service.ts
+++ src/UI/src/app/core/services/application-service.ts
@@ -1,24 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Application} from '../models/application';
 import { Observable } from 'rxjs';
+import { Application } from '../models/application';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class ApplicationService {
+  private readonly applicationUrl = 'http://localhost:8080/applications';
 
-  private readonly applicationUrl: string;
-
-  constructor(private http: HttpClient) {
-    this.applicationUrl = "http://localhost:8080/applications";
-  }
+  constructor(private http: HttpClient) {}
 
   public findAll(): Observable<Application[]> {
     return this.http.get<Application[]>(this.applicationUrl);
   }
 
-  public save(application: Application) {
+  public get(id: number): Observable<Application> {
+    return this.http.get<Application>(`${this.applicationUrl}/${id}`);
+  }
+
+  public save(application: Application): Observable<Application> {
     return this.http.post<Application>(this.applicationUrl, application);
+  }
+
+  public update(id: number, application: Application): Observable<Application> {
+    return this.http.put<Application>(`${this.applicationUrl}/${id}`, application);
+  }
+
+  public delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.applicationUrl}/${id}`);
+  }
+
+  public findByCompany(company: string): Observable<Application[]> {
+    return this.http.get<Application[]>(`${this.applicationUrl}?company=${encodeURIComponent(company)}`);
   }
 }
