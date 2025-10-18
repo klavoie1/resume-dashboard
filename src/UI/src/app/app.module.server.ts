90efd37c7b6d3796cd1f6797f002958fdf166c7b
klavoie <klavoi8@wgu.edu>
2025-10-17 20:54:20 -0400

    Fixed imports and bootstraps, added Zoneless

--- src/UI/src/app/app.module.server.ts
+++ src/UI/src/app/app.module.server.ts
@@ -1,14 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, provideZonelessChangeDetection } from '@angular/core';
 import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { App } from './app';
 import { AppModule } from './app-module';
 import { serverRoutes } from './app.routes.server';
 import {DashboardModule} from './features/dashboard/dashboard-module';
 import {Dashboard} from './features/dashboard/dashboard';
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
-  imports: [DashboardModule],
-  providers: [provideServerRendering(withRoutes(serverRoutes))],
-  bootstrap: [Dashboard],
+  imports: [
+    AppModule
+  ],
+  providers: [
+    provideServerRendering(withRoutes(serverRoutes)),
+    provideZonelessChangeDetection()
+  ],
+  bootstrap: [App],
 })
 export class AppServerModule {}
