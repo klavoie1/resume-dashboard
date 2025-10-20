39eb497b2dba38ae2765b354e6d017a84adaca8c
klavoie <klavoi8@wgu.edu>
2025-10-20 16:09:45 -0400

    Added functionality for Editing and deleting applications

--- src/UI/src/app/features/edit-application/edit-application.ts
+++ src/UI/src/app/features/edit-application/edit-application.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Application } from '../../core/models/application';
+import { ApplicationService } from '../../core/services/application-service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-application',
@@ -7,5 +10,83 @@ import { Component } from '@angular/core';
   styleUrl: './edit-application.css'
 })
 export class EditApplication {
+  application: Application = {
+    jobTitle: '',
+    company: '',
+    status: 'PENDING',
+    submissionDate: ''
+  };
 
+  isSubmitting = false;
+  isLoading = false;
+  error?: string;
+
+  constructor(
+    private applicationService: ApplicationService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.loadApplication();
+  }
+
+  private loadApplication() {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    const id = idParam ? Number(idParam) : NaN;
+
+    if (!id || Number.isNaN(id)) {
+      this.error = 'Invalid application id';
+      return;
+    }
+
+    this.isLoading = true;
+    this.applicationService.get(id).subscribe({
+      next: (app) => {
+        this.application = app;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.error = 'Failed to load application';
+        this.isLoading = false;
+      }
+    });
+  }
+
+  onSubmit() {
+    this.error = undefined;
+    this.isSubmitting = true;
+    if (this.application.id != null) {
+      this.applicationService.update(this.application.id, this.application).subscribe({
+        next: () => {
+          this.isSubmitting = false;
+          this.router.navigate(['/dashboard']);
+        },
+        error: (err) => {
+          this.isSubmitting = false;
+          this.error = 'Failed to edit application';
+          console.error(err);
+        }
+      });
+    }
+  }
+
+  onDelete() {
+    if (!this.application.id) return;
+    const yes = confirm('Are you sure you want to delete this application?');
+    if (!yes) return;
+    this.error = undefined;
+
+    this.isSubmitting = true;
+    this.applicationService.delete(this.application.id).subscribe({
+      next: () => {
+        this.isSubmitting = false;
+        this.router.navigate(['/dashboard'])
+      },
+      error: (err) => {
+        this.isSubmitting = false;
+        this.error = 'Failed to delete application';
+        console.error(err);
+      }
+    });
+  }
 }
